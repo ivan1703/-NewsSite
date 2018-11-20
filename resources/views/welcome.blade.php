46054e71fb6b1c7ee8949427a25f69f9ed57874c
Ivan Balashov <ivan.programmer@bk.ru>
2018-11-20 06:27:49 +0000

    new file and new db

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
     <body>
         <!--навигация по сайту (bootstrap 4)-->
   <nav class="navbar navbar-expand-lg navbar-light bg-info">
-  <a class="navbar-brand" href="/">News</a>
+  <a class="navbar-brand" href="#">News</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -18,10 +18,10 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item">
-        <a class="nav-link" href="">Лента</a>
+        <a class="nav-link" href="{{asset("/")}}">Лента</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link " href="">Предложить новость</a>
+        <a class="nav-link " href="{{asset("CreateNews")}}">Предложить новость</a>
       </li>
     </ul>
   </div>
